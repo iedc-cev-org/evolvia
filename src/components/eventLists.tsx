fd54d6df28e23b23ea8b65bef8fdfddfc5500554
Amarnath  Sooraj <166597458+AmarnathSooraj@users.noreply.github.com>
2025-10-01 23:35:10 +0530

    Update eventLists.tsx

--- src/components/eventLists.tsx
+++ src/components/eventLists.tsx
@@ -60,7 +60,7 @@ export const Events = [
     name: "PitchBox - Idea pitching competition",
     image:"/events/pitchbox.webp",
     spec:"Deadline for submission: Oct 05 2025",
-    dateTime: "Oct 08 2025 | 1:30 PM - 4 PM",
+    dateTime: "Oct 08 2025",
     venue: "Astra | CEV Premises", 
     link:"https://snaptiqz.com/event/d3CX6qg5nslalaXoNf_p6",
   },
