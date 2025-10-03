359aa53ff5e2e5f2828d977253b009b04cd5d57f
souravkl11 <souravkalathingals@gmail.com>
2025-10-03 12:26:40 +0530

    added venture way + used webp equivalents

--- src/components/eventLists.tsx
+++ src/components/eventLists.tsx
@@ -1,27 +1,27 @@
-export const preEvents=[
+export const preEvents = [
   {
-    name:"How to build a Startup",
+    name: "How to build a Startup",
     isCompleted: true,
-    image:'/pre-events/franklin.webp',
-    completed_image:'/pre-events/franklin_completed.webp',
+    image: '/pre-events/franklin.webp',
+    completed_image: '/pre-events/franklin_completed.webp',
     description: "",
-    link:""
+    link: ""
   },
   {
-    name:"What's Vibe Coding",
+    name: "What's Vibe Coding",
     isCompleted: true,
-    image:'/pre-events/vibecoding.webp',
-    completed_image:'/pre-events/vibecoding_completed.webp',
+    image: '/pre-events/vibecoding.webp',
+    completed_image: '/pre-events/vibecoding_completed.webp',
     description: "",
-    link:""
+    link: ""
   },
   {
-    name:"What's Supabase",
+    name: "What's Supabase",
     isCompleted: true,
-    image:'/pre-events/supabase.webp',
-    completed_image:'/pre-events/supabase_completed.webp',
+    image: '/pre-events/supabase.webp',
+    completed_image: '/pre-events/supabase_completed.webp',
     description: "",
-    link:""
+    link: ""
   }
 ]
 
@@ -29,92 +29,89 @@ export const preEvents=[
 
 export const Events = [
   {
-    id:1,
+    id: 1,
     name: "IEDC Alumni Interaction Meet",
-    image:"/events/alumini.webp",
-    spec:"Only for pass out Students",
+    image: "/events/alumini.webp",
+    spec: "Only for pass out Students",
     dateTime: "Oct 08 2025 | 9 AM - 4 PM",
     venue: "College Premises",
-    link:"https://snaptiqz.com/event/iuqDD8FbT7rFE5UvJTu5j",
+    link: "https://snaptiqz.com/event/iuqDD8FbT7rFE5UvJTu5j",
   },
   {
-    id:2,
+    id: 2,
     name: "BIT'BURST 2.0",
-    image:"/events/bitburst.webp",
-    spec:"Exclusively for CEV Students",
+    image: "/events/bitburst.webp",
+    spec: "Exclusively for CEV Students",
     dateTime: "Oct 06 - 07 2025 | 4 PM | 24 hrs",
     venue: "CCF, Programing Lab",
-    link:"https://snaptiqz.com/event/pCn_980UQs5EC9rp3SLfu",
+    link: "https://snaptiqz.com/event/pCn_980UQs5EC9rp3SLfu",
   },
   {
-    id:3,
+    id: 3,
     name: "Visio - AI image & video exhibition",
-    image:"/events/visio_ai.webp",
-    spec:"Open for all students",
-     dateTime: "Oct 08 2025 | 9 AM - 4 PM",
-    venue: "College Premises", 
-    link:"https://snaptiqz.com/event/AwEd2rU3f91edjdHIVwow",
+    image: "/events/visio_ai.webp",
+    spec: "Open for all students",
+    dateTime: "Oct 08 2025 | 9 AM - 4 PM",
+    venue: "College Premises",
+    link: "https://snaptiqz.com/event/AwEd2rU3f91edjdHIVwow",
   },
   {
-    id:4,
+    id: 4,
     name: "PitchBox - Idea pitching competition",
-    image:"/events/pitchbox.webp",
-    spec:"Deadline for submission: Oct 05 2025",
+    image: "/events/pitchbox.webp",
+    spec: "Deadline for submission: Oct 05 2025",
     dateTime: "Oct 08 2025",
-    venue: "Astra | CEV Premises", 
-    link:"https://snaptiqz.com/event/d3CX6qg5nslalaXoNf_p6",
+    venue: "Astra | CEV Premises",
+    link: "https://snaptiqz.com/event/d3CX6qg5nslalaXoNf_p6",
   },
-   
+
   {
-    id:5,
+    id: 5,
     name: "Hack 4 Ease",
-    image:"/events/hack4ease.webp",
-    spec:"",
+    image: "/events/hack4ease.webp",
+    spec: "",
     dateTime: "Oct 08 2025 | 10 AM - 1 PM",
     venue: "Eclipse | CEV Premises",
-    link:"https://snaptiqz.com/event/jZ4EWWS_GU-KVI5BEVymZ",
+    link: "https://snaptiqz.com/event/jZ4EWWS_GU-KVI5BEVymZ",
     // link:"",
   },
 
   {
-    id:6,
+    id: 6,
     name: "Quizzard",
-    image:"/events/IMG-20251003-WA0000.jpg",
-    spec:"",
+    image: "/events/quizzard_college.webp",
+    spec: "",
     dateTime: "Oct 08 2025 | 1:30 PM - 3 PM",
     venue: "Orion | CEV Premises",
-    link:"  https://snaptiqz.com/event/q_jbt1fDaUtjfWSqPK7DG",
+    link: "  https://snaptiqz.com/event/q_jbt1fDaUtjfWSqPK7DG",
   },
-{
-    id:7,
+  {
+    id: 7,
     name: "IEDC Leads Meet",
     image: "/events/leadsmeet.webp",
     spec: "",
     dateTime: "Oct 08 2025",
     venue: "CEV Premises",
-    link:"  https://snaptiqz.com/event/SDkHpsY8wg9J9RleiPFP9",
+    link: "  https://snaptiqz.com/event/SDkHpsY8wg9J9RleiPFP9",
   },
-  
+
   {
-    id:8,
+    id: 8,
     name: "Quizzard - School",
-    image:"/events/IMG-20251003-WA0001.jpg",
-    spec:"Exclusively for School Students",
+    image: "/events/quizzards_school.webp",
+    spec: "Exclusively for School Students",
     dateTime: "08 Oct 2025 | 10:00 AM- 12:30 PM",
     venue: "TBA",
-     link:"https://snaptiqz.com/event/QqZXqSQO6Pj5YVX8H1Gb6",
+    link: "https://snaptiqz.com/event/QqZXqSQO6Pj5YVX8H1Gb6",
   },
-  /*
   {
-    id:8,
+    id: 9,
     name: "Venture Way",
-    image:"",
-    spec:"unlimited seats",
+    image: "/events/ventureway.webp",
+    spec: "unlimited seats",
     dateTime: "TBA",
     venue: "TBA",
-     link:"https://snaptiqz.com/event/QqZXqSQO6Pj5YVX8H1Gb6",
+    // link: "https://snaptiqz.com/event/QqZXqSQO6Pj5YVX8H1Gb6",
   },
-  */
-  
 ];
 
